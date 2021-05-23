379c7a4267d1293e5c58572da49af632c73fb309
fernandocgomez <fernandocgomez@live.com>
2021-05-23 11:46:49 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Webpack Boilerplate
+This is a webpack with typescript boiler plate based on the webpack docs.
+
+# Installation
+Use npm to download the needed node_modules.
+
+```bash 
+npm install
+```
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+// Plugins
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+
+module.exports = {
+    mode: "production",
+    entry: {
+        index: './src/index.ts',
+    },
+    // devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'My Web Pack',
+        }),
+        new BrowserSyncPlugin({
+            host: 'localhost',
+            port: 3000,
+            files: ['./dist/*.html'],
+            server: { baseDir: ['dist'] }
+        })
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+};
